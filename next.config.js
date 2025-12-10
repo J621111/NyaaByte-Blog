79028fbaa112e35f50409f9c448e39ac0e0fdaaf
NyaaByte <1500532940@qq.com>
2025-12-10 21:13:06 +0800

    fix: configuration change

--- next.config.js
+++ next.config.js
@@ -14,10 +14,18 @@ const config = {
 
   // 图片配置 (使用 next/image)
   images: {
+    dangerouslyAllowSVG: true, // 允许 SVG 格式图片
     unoptimized: true, // 禁用图片优化
-    domains: ["cdn.jsdelivr.net"], // 允许加载的外部域名
+    contentDispositionType: 'attachment', // 设置内容处置类型为 attachment
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;", // 设置内容安全策略
     // 使用 remotePatterns 字段来指定允许远程图片的域名
     remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.jsdelivr.net',
+        port: '',
+        pathname: '/**',
+      },
       { hostname: 'localhost' },
       { hostname: '127.0.0.1' },
       { hostname: 'www.nyaabyte.com' }, 
